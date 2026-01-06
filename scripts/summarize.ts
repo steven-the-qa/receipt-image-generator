ec9eb167c88de9982a2e7796d68c50b144f3a3c6
Steven Boutcher <boutchersj@gmail.com>
2026-01-06 04:00:29 -0600

    Sort the durations array once instead to DRY up the solution

--- scripts/summarize.ts
+++ scripts/summarize.ts
@@ -1,16 +1,17 @@
 import * as testResults from '../results.json';
 
-// Summary
-// {
-//     averageDuration: number (milliseconds)
-//     slowestTest: string (test name)
-//     fastestTest: string (test name)
-// }
+// Find the fastest and slowest tests in the `formatReceipt.spec.ts` file
+// Also, calculate the average duration of all tests in the file
+
+type TestDuration = {
+    name: string,
+    duration: number
+}
 
 interface TestSummary {
     averageDuration: number // (milliseconds)
-    slowestTest: string // (test name)
-    fastestTest: string // (test name)
+    slowestTest: TestDuration
+    fastestTest: TestDuration
 }
 
 function summarizeResults(results: any): TestSummary {
@@ -21,17 +22,22 @@ function summarizeResults(results: any): TestSummary {
         acc.testDurations.push({ name: spec.title, duration: testDuration });
 
         return acc;
-    }, { averageDuration: 0, slowestTest: '', fastestTest: '', totalDuration: 0, testDurations: [] } as TestSummary & { totalDuration: number, testDurations: [] });
+    }, {
+        averageDuration: 0,
+        slowestTest: { name: '', duration: 0 },
+        fastestTest: { name: '', duration: 0 },
+        totalDuration: 0,
+        testDurations: []
+    } as TestSummary & { totalDuration: number, testDurations: TestDuration[] });
 
     summary.averageDuration = Number((summary.totalDuration / formatReceiptSuite.specs.length).toFixed(2));
 
-    summary.fastestTest = summary.testDurations.sort((a, b) => {
+    summary.testDurations.sort((a, b) => {
         return b.duration - a.duration;
-    }).pop().name;
+    });
 
-    summary.slowestTest = summary.testDurations.sort((a, b) => {
-        return a.duration - b.duration;
-    }).pop().name;
+    summary.fastestTest = summary.testDurations.pop();
+    summary.slowestTest = summary.testDurations.shift();
 
     delete summary.totalDuration
     delete summary.testDurations
